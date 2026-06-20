d8468e787404ec23f24263afe11ffdd237030d0e
Finney Bado <finney.bado@icloud.com>
2026-06-19 23:02:07 -0400

    feat: create basic Node model

---
+++ datastructure/Node.java
@@ -0,0 +1,14 @@
+public class Node<T> {
+
+    // fields
+    T data;
+    Node<T> next;
+
+    // Constructor
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    // getters and setters to add
+}
