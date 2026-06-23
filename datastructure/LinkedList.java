cee0314778a6484d7a14ce47e3c7640a3f61d42a
Finney Bado <finney.bado@icloud.com>
2026-06-22 21:18:41 -0400

    feat: create a basic linked list model

---
+++ datastructure/LinkedList.java
@@ -0,0 +1,46 @@
+public class LinkedList{
+
+    //fields
+    private Node<T> head;
+    private int size;
+
+    // constructor
+    public LinkedList(){
+        this.head = null;
+        this.size = 0
+    }
+
+    // Getters and Setters
+    public Node<T> getHead(){
+        return this.head;
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    public void incrementSize
+
+    // main interface
+    public void insert(Node<T> node,int index){
+
+    }
+
+    public T getNode(int index){
+
+    }
+
+    public void setNode(T data,int index){
+
+    }
+
+    public T removeNode(int index){
+
+    }
+
+    public void increment
+
+
+
+
+}
