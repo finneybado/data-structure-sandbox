650a3c6a3fbe041707aa1c70765877d3eff27dca
Finney Bado <finney.bado@icloud.com>
2026-06-23 21:09:15 -0400

    feat: implement node inserting method in linked list

--- datastructure/LinkedList.java
+++ datastructure/LinkedList.java
@@ -15,9 +15,15 @@ public class LinkedList{
         return this.head;
     }
 
+    private void setHead(Node<T> node){
+        this.head = node;
+    }
+
+
     public int size(){
         return this.size;
     }
+
 
     private void incrementSize(){
         this.size++;
@@ -29,12 +35,48 @@ public class LinkedList{
 
 
     // main interface
-    public void insertNode(Node<T> node,int index){
+    public void insertNode(T data,int index){
+        if (index==0){
+
+            if getHead() == null{
+              this.setHead(data);
+            }
+            Node<T> temp = new Node(data);
+            temp.setNext(head.getNext());
+            setHead(temp);
+
+        }else if( index<0 ||index >=this.size()){
+            System.out.println(">> invalid index : out of bounds ")
+        }else{
+
+                Node<T> firstTracker = getHead();
+                Node<T> secondTracker = firstTracker.getNext();
+
+                for(int _=0;_<index;_++){
+                    firstTracker = secondTracker;
+                    secondTracker = secondTracker.getNext();
+                }
+
+                if (secondTracker == null){
+
+                }
+
+                tracker.setNext(new Node(data));
+                incrementSize();
+        }
 
     }
 
-    public void addNode(Node<T> node,int index){
-        <>
+    public void addNode(T data){
+        Node<T> tracker = getHead();
+
+        while tracker.getNext() != null{
+            tracker = tracker.getNext();
+        }
+
+        tracker.setNext(new Node(data));
+        incrementSize();
+
     }
 
     public T getNode(int index){
